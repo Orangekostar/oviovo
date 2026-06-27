3bcc5d16ffefa73b3592b009bc2ba4ede4941f7f
orangekostar <406947562@qq.com>
2026-06-27 19:38:02 +0800

    fix: handle int items in new_candidate_objects

--- src/pipelines/main_pipeline.py
+++ src/pipelines/main_pipeline.py
@@ -795,7 +795,7 @@ class Pipeline:
                 list(item) for item in getattr(self.dynamic_maintenance, "last_moved_object_ids", [])
             ],
             "new_candidate_objects": [
-                list(item) for item in getattr(self.dynamic_maintenance, "last_new_candidate_ids", [])
+                (int(item), "") for item in getattr(self.dynamic_maintenance, "last_new_candidate_ids", [])
             ],
             "ghost_object_count": int(
                 sum(1 for o in self.state.objects.values()
